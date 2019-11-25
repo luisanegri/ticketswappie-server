5c071228b35b2620de884e5f70f9a76d2f739181
luisanegri <luisaanegri@gmail.com>
2019-11-25 11:14:14 +0100

    add allowNull to model

--- events/model.js
+++ events/model.js
@@ -2,11 +2,26 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Event = db.define('event', {
-  name: Sequelize.STRING,
-  description: Sequelize.STRING,
-  image: Sequelize.STRING,
-  start_date: Sequelize.STRING,
-  end_date: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  start_date: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  end_date: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
 });
 
 module.exports = Event;
