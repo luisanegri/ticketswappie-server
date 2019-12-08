527e80b2d1e507a83d0e03de217e976e6e792d46
luisanegri <luisaanegri@gmail.com>
2019-12-08 21:27:16 +0100

    end

--- tickets/algorithm.js
+++ tickets/algorithm.js
@@ -3,17 +3,16 @@
 //   let risk = 0;
 
 //   try {
-//     // primeira tarefa
 //     const userTickets = await Ticket.findAll({
 //       where: { userId: ticket.userId }
 //     });
-
-//     console.log('usertickets', userTickets);
 
 //     const countUserTickets = userTickets.length;
 //     if (countUserTickets === 1) {
 //       risk += 10;
 //     }
+
+//     console.log('risk first', risk);
 
 //     // segunda tarefa
 //     const eventTickets = await Ticket.findAll({
@@ -25,33 +24,42 @@
 //         return (a += b.price);
 //       }, 0) / eventTickets.length;
 
-//     if (ticket.price > avgTicketPrice) {
-//       const difference1 = ticket.price - avgTicketPrice;
-//       const diffPercentage = ((difference1 / avgTicketPrice) * 100).toFixed(2);
-//       if (diffPercentage > 10) {
-//         risk -= 10;
-//       } else {
-//         risk -= diffPercentage;
-//       }
-//     } else if (ticket.price <= avgTicketPrice) {
+//     console.log('avg ticket price', avgTicketPrice);
+
+//     if (ticket.price <= avgTicketPrice) {
 //       const difference2 = avgTicketPrice - ticket.price;
 //       const diffPercentage = ((difference2 / avgTicketPrice) * 100).toFixed(2);
-//       risk += diffPercentage;
+//       risk = Number(risk) + Number(diffPercentage);
+//     } else if (ticket.price > avgTicketPrice) {
+//       const difference1 = ticket.price - avgTicketPrice;
+//       const diffPercentage = ((difference1 / avgTicketPrice) * 100).toFixed(2);
+//       console.log('diff per', diffPercentage);
+//       if (diffPercentage > 10) {
+//         risk = Number(risk) - 10;
+//       } else {
+//         risk = Number(risk) - Number(diffPercentage);
+//       }
 //     }
+//     console.log('total', risk);
 
 //     // terceira tarefa
 //     const timeCreated = ticket.createdAt.getHours();
+//     console.log('time create', timeCreated);
 //     if (timeCreated >= 9 && timeCreated <= 17) {
-//       risk -= 10;
+//       risk = Number(risk) - 10;
 //     } else {
-//       risk += 10;
+//       risk = Number(risk) + 10;
 //     }
+//     console.log('risk time', risk);
 
 //     // quarta tarefa
-//     const countComments = ticket.comments;
-//     if (countComments.length > 3) {
-//       risk += 5;
+//     const ticketComments = ticket.comments;
+//     // console.log('ticket comments', ticketComments);
+//     if (ticketComments.length === 1) {
+//       risk = Number(risk) + 60;
 //     }
+
+//     console.log('ticket comment risk', risk);
 
 //     // quinta tarefa
 //     if (risk < 5) {
@@ -60,8 +68,10 @@
 //       risk = 95;
 //     }
 
-//     ticket.risk = risk;
-//     return ticket;
+//     // console.log('end', risk);
+
+//     ticket.risk = Math.round(risk);
+//     res.json(ticket);
 //   } catch (err) {
 //     console.log(err);
 //   }
--- tickets/router.js
+++ tickets/router.js
@@ -3,7 +3,6 @@ const Ticket = require('./model');
 const router = new Router();
 const auth = require('../auth/middleware');
 const Comment = require('../comment/model');
-const calcTicket = require('./algorithm');
 
 router.post('/event/:eventId/ticket', auth, (req, res, next) => {
   Ticket.create({
@@ -16,8 +15,7 @@ router.post('/event/:eventId/ticket', auth, (req, res, next) => {
     .catch(error => next(error));
 });
 
-// get all tickets from specific event
-router.get('/event/:eventId/ticket', (req, res, next) => {
+router.get('/event/:eventId/ticket', async (req, res, next) => {
   Ticket.findAll({ include: [Comment], where: { eventId: req.params.eventId } })
     .then(ticket => res.send(ticket))
     .catch(error => next(error));
@@ -26,23 +24,21 @@ router.get('/event/:eventId/ticket', (req, res, next) => {
 // get single ticket
 router.get('/ticket/:ticketId', async (req, res) => {
   try {
-    risk = 0;
-
     const ticket = await Ticket.findOne({
       include: [Comment],
       where: { id: req.params.ticketId }
     });
+
+    risk = 0;
 
     const userTickets = await Ticket.findAll({
       where: { userId: ticket.userId }
     });
 
     const countUserTickets = userTickets.length;
-    if (countUserTickets > 1) {
+    if (countUserTickets === 1) {
       risk += 10;
     }
-
-    console.log('risk first', risk);
 
     // segunda tarefa
     const eventTickets = await Ticket.findAll({
@@ -54,15 +50,10 @@ router.get('/ticket/:ticketId', async (req, res) => {
         return (a += b.price);
       }, 0) / eventTickets.length;
 
-    console.log('avg ticket price', avgTicketPrice);
-    console.log('risk before second conditions', risk);
-
     if (ticket.price <= avgTicketPrice) {
       const difference2 = avgTicketPrice - ticket.price;
       const diffPercentage = ((difference2 / avgTicketPrice) * 100).toFixed(2);
-      console.log('typeof', typeof diffPercentage);
       risk = Number(risk) + Number(diffPercentage);
-      console.log('total', risk);
     } else if (ticket.price > avgTicketPrice) {
       const difference1 = ticket.price - avgTicketPrice;
       const diffPercentage = ((difference1 / avgTicketPrice) * 100).toFixed(2);
@@ -75,7 +66,6 @@ router.get('/ticket/:ticketId', async (req, res) => {
 
     // terceira tarefa
     const timeCreated = ticket.createdAt.getHours();
-    console.log('time create', timeCreated);
     if (timeCreated >= 9 && timeCreated <= 17) {
       risk = Number(risk) - 10;
     } else {
@@ -84,12 +74,9 @@ router.get('/ticket/:ticketId', async (req, res) => {
 
     // quarta tarefa
     const ticketComments = ticket.comments;
-    console.log('ticket comments', ticketComments);
-    if (ticketComments.length === 1) {
-      risk = Number(risk) + 60;
+    if (ticketComments.length > 3) {
+      risk = Number(risk) + 10;
     }
-
-    // console.log('ticket comment risk', risk);
 
     // quinta tarefa
     if (risk < 5) {
@@ -98,17 +85,16 @@ router.get('/ticket/:ticketId', async (req, res) => {
       risk = 95;
     }
 
-    // console.log('end', risk);
-
-    ticket.risk = risk;
+    ticket.risk = Math.round(risk);
     res.json(ticket);
   } catch (err) {
     console.log(err);
   }
 });
 
-router.put('event/eventId/ticket/:ticketId', (req, res, next) => {
-  Ticket.findByPk(req.params.ticketId)
+router.put('ticket/:ticketId', (req, res, next) => {
+  console.log('tickeeeeeeeeet are you there?');
+  Ticket.findByPk(parseInt(req.params.ticketId))
     .then(ticket => ticket.update(req.body))
     .then(ticket => res.send(ticket))
     .catch(error => next(error));
